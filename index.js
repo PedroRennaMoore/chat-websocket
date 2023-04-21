07b7cb7d27f78a9996f05f32554de1abd8d5ca98
Pedro Renna Moore <moorerpedro@gmail.com>
2023-04-21 21:05:54 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {Server} from 'socket.io'
 
-const io = new Server(80, {
+const io = new Server(8000, {
     cors: {
         origin: "https://chat-pedromoore.web.app",
         methods: ["GET", "POST"]

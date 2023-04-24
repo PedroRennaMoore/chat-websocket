15460a02c4f0d6b6d8d224db158929a77d7cde0c
Pedro Renna Moore <moorerpedro@gmail.com>
2023-04-24 11:22:10 +0100

    changes

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 import {Server} from 'socket.io'
 import http  from 'http'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors)
 const server = http.createServer(app)
 
 const PORT = process.env.PORT || 3003

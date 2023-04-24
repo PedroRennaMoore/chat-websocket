6859e268ce5aa83b50ed79b1a49353861e07a3bc
Pedro Renna Moore <moorerpedro@gmail.com>
2023-04-24 10:43:34 +0100

    changes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 import {Server} from 'socket.io'
 import express from 'express'
 import {createServer} from 'http'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 const server = createServer(app)
 
@@ -11,8 +14,7 @@ const PORT = process.env.PORT || 3000
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+        origin: "*"
     }
 })
 
